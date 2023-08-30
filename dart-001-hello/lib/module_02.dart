cb01aaead3ba060877b6d8bb705d67f0f54361f6
callor <callor@callor.com>
2023-08-30 11:52:01 +0900

    R

--- dart-001-hello/lib/module_02.dart
+++ dart-001-hello/lib/module_02.dart
@@ -1,7 +1,9 @@
 String sayHello(String? name, int? age, String? nation) =>
     "안녕 $name 야, 당신의 나이는 $age 이고, 고향은 $nation 이야!!";
 
-String sayHelloNameParams({String? name, int? age, String? nation}) =>
+/// sayHelloNameParams("대한민국")
+String sayHelloNameParams(String contry,
+        {String? name, int? age, String? nation}) =>
     "안녕 $name 야, 당신의 나이는 $age 이고, 고향은 $nation 이야!!";
 
 
