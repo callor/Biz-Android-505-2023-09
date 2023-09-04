630a808b5a2d4ee6ffb5cdd899acf9e0e6c08508
callor <callor@callor.com>
2023-09-04 09:51:04 +0900

    todoV2

--- flutter-006-todolist-v2/lib/main.dart
+++ flutter-006-todolist-v2/lib/main.dart
@@ -25,30 +25,184 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPage extends State<StartPage> {
+  /// **Controller
+  /// flutter 의 대부분의 Widget 에는 위젯Controler 라는  객체가 있다
+  /// HTML 의 id 속성에 해당하는 객체이다
+  var inputController = TextEditingController();
+  var todoContent = "";
   var todoList = [];
-  final _delConfirm = false;
-
-  // var todoList = [
-  //   Todo(
-  //     sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
-  //     stime: DateFormat("HH:ss:mm").format(DateTime.now()),
-  //     content: "오늘은 금요일",
-  //     complete: false,
-  //   ),
-  //   Todo(
-  //     sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
-  //     stime: DateFormat("HH:ss:mm").format(DateTime.now()),
-  //     content: "Flutter 프로젝트",
-  //     complete: false,
-  //   )
-  // ];
-
   Todo getTodo(String content) {
     return Todo(
       sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
       stime: DateFormat("HH:ss:mm").format(DateTime.now()),
       content: content,
       complete: false,
+    );
+  }
+
+  /// Scaffold
+  /// 처음 화면의 layout 을 꾸밀때 사용하는 Widget이다
+  /// appBar, body, bottomSheet 가 있는데
+  /// appBar 는 화면의 머릿글에 해당하는 부분
+  /// body 는 본문(중앙)에 해당하는 부분
+  /// bottomSheet 는 화면의 footer(꼬릿글)에 해당하는 부분
+  /// body 는 보통 데이터를 표현하는 부분, 여긴 Scroll 이 일어나는 부분
+  /// appBar, bottomSheet 는 body 가 변화되더라도 Scroll 이 되지 않는 부분
+  /// bttomSheet 는 입력화면에서 소프트 키보드가 나타 나면 키보드 위에
+  ///     표현되는 부분
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Image.asset(
+          "images/profile.jpg",
+          fit: BoxFit.fill,
+        ),
+        title: const Text("나의 TodoList"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) => alertDialog(context),
+                );
+              },
+              icon: const Icon(
+                Icons.add_alarm,
+              ))
+        ],
+      ),
+      bottomSheet: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  // TextField 에 id(Controller) 를 설정하기
+                  controller: inputController,
+                  onChanged: (value) => setState(() {
+                    todoContent = value;
+                  }),
+                  decoration: InputDecoration(
+                    prefix: const SizedBox(
+                      width: 20,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () => inputController.clear,
+                      icon: const Icon(Icons.clear),
+                    ),
+                    hintText: "할일을 입력해 주세요",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.send_outlined),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: todoListView(),
+    );
+  }
+
+  /// ListView return type 을 Widget으로 변경하기
+  /// 모든 Widget 은 가장 상위 클래스인 Widget 을 상속받고 있기 때문에
+  /// 모든 Widget 의 return type 은 Widget 으로 설정하여도 된다
+  Widget todoListView() {
+    return ListView.builder(
+      itemCount: todoList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          onTap: () {},
+          splashColor: const Color.fromARGB(255, 180, 20, 30).withOpacity(0.5),
+          title: Dismissible(
+            key: Key(todoList[index].content),
+            background: Container(
+              margin: const EdgeInsets.all(8),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              color: Colors.green,
+              alignment: Alignment.centerLeft,
+              child: const Icon(
+                Icons.save,
+                size: 36,
+                color: Colors.white,
+              ),
+            ),
+            secondaryBackground: Container(
+              margin: const EdgeInsets.all(8),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              child: const Icon(
+                Icons.delete,
+                size: 36,
+                color: Colors.white,
+              ),
+            ),
+            // 사라지기 전의 event
+            confirmDismiss: (direction) {
+              return showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text("정말삭제할까요?"),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(true);
+                      },
+                      child: const Text("예"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                      child: const Text("아니오"),
+                    ),
+                  ],
+                ),
+              );
+              // return Future.value(_delConfirm);
+            },
+            // 사라진 후 event
+            onDismissed: (direction) {
+              setState(() {
+                todoList.removeAt(index);
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(todoList[index].sdate),
+                      Text(todoList[index].stime),
+                    ],
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        todoList[index].content,
+                        style:
+                            const TextStyle(fontSize: 20, color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -79,128 +233,4 @@ class _StartPage extends State<StartPage> {
           )
         ],
       );
-
-  @override
-  Widget build(BuildContext context) {
-    // State 클래스 내의 함수를 실행하여 결과를 가져온 후 todoList 를 만든다
-    // 이때 클래스 내의 함수를 호출하여 다른 변수를 만드는 경우
-    // 이 코드는 build 함수 내부에서 실행해야 한다
-    //  todoList.add = [
-    //   getTodo("플러터 프로젝트"),
-    //   getTodo("오늘은 금요일"),
-    //   getTodo("내일은 토요일 뭐할까"),
-    // ];
-
-    return Scaffold(
-      appBar: AppBar(
-        leading: Image.asset(
-          "images/profile.jpg",
-          fit: BoxFit.fill,
-        ),
-        title: const Text("나의 TodoList"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                showDialog(
-                  barrierDismissible: false,
-                  context: context,
-                  builder: (context) => alertDialog(context),
-                );
-              },
-              icon: const Icon(
-                Icons.add_alarm,
-              ))
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            onTap: () {},
-            splashColor:
-                const Color.fromARGB(255, 180, 20, 30).withOpacity(0.5),
-            title: Dismissible(
-              key: Key(todoList[index].content),
-              background: Container(
-                margin: const EdgeInsets.all(8),
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                color: Colors.green,
-                alignment: Alignment.centerLeft,
-                child: const Icon(
-                  Icons.save,
-                  size: 36,
-                  color: Colors.white,
-                ),
-              ),
-              secondaryBackground: Container(
-                margin: const EdgeInsets.all(8),
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                color: Colors.red,
-                alignment: Alignment.centerRight,
-                child: const Icon(
-                  Icons.delete,
-                  size: 36,
-                  color: Colors.white,
-                ),
-              ),
-              // 사라지기 전의 event
-              confirmDismiss: (direction) {
-                return showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text("정말삭제할까요?"),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop(true);
-                        },
-                        child: const Text("예"),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop(false);
-                        },
-                        child: const Text("아니오"),
-                      ),
-                    ],
-                  ),
-                );
-                // return Future.value(_delConfirm);
-              },
-              // 사라진 후 event
-              onDismissed: (direction) {
-                setState(() {
-                  todoList.removeAt(index);
-                });
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(todoList[index].sdate),
-                        Text(todoList[index].stime),
-                      ],
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          todoList[index].content,
-                          style:
-                              const TextStyle(fontSize: 20, color: Colors.blue),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
 }
