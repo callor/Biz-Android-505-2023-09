c47920470a523c98f63ced43e9cef6118183766f
callor <callor@callor.com>
2023-09-11 15:54:12 +0900

    naver

--- flutter-010-movie/lib/main.dart
+++ flutter-010-movie/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:movie/api/naver_open_api.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
@@ -57,13 +58,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    NaverAPI().loadMovie();
+  }
+
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
---
+++ flutter-010-movie/naver.md
@@ -0,0 +1,11 @@
+```xml
+<rss>
+    <channel>
+        <lastBuildDate></lastBuildDate>
+        <total></total>
+        <start></start>
+        <display></display>
+        <item></item>
+    </channel>
+</rss>
+```
